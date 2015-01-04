6451a51515550d84a172f0da0f2fa91842947d56
Ben Adam <ben.adam@laneterralever.com>
2015-01-03 21:20:00 -0700

    Added mobile nav and project content

--- .build/static/js/main.js
+++ .build/static/js/main.js
@@ -11,6 +11,18 @@ $(document).ready(function(){
     $('.social-icon').toggle( 'slide' );
   });
 
+  $('.navigation-toggle').on('click',function() {
+
+   if($('.navigation-target').hasClass('clicked')){
+     $('.navigation-target').removeClass('clicked');
+     $('.dropdown-menu').slideUp();
+   }
+   else{
+     $('.navigation-target').addClass('clicked');
+     $('.dropdown-menu').slideDown();
+   }
+ });
+
   //Fade In Images On Load 
   $('img').load(function() {
     $(this).fadeIn('slow');
