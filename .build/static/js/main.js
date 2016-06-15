20c354704b18fb8b29c64dd84313fbb2523edb41
Ben Adam <bxadam@godaddy.com>
2016-06-15 16:54:12 -0700

    Adding so much stuffs

--- .build/static/js/main.js
+++ .build/static/js/main.js
@@ -1,10 +1,12 @@
 //Set Date to Current Year
 $('#footer-date').html(new Date().getFullYear());
 
-//Hide all of the content before the page loads 
-$(".content").css('visibility','hidden');
+// 
+$('html').hide()
 
 $(document).ready(function(){
+
+  $('html').fadeIn(300)
 	
 	// Social toggle 
   $('#social-click').click(function(){
@@ -13,23 +15,42 @@ $(document).ready(function(){
 
   $('.navigation-toggle').on('click',function() {
 
-   if($('.navigation-target').hasClass('clicked')){
-     $('.navigation-target').removeClass('clicked');
-     $('.dropdown-menu').slideUp();
-   }
-   else{
-     $('.navigation-target').addClass('clicked');
-     $('.dropdown-menu').slideDown();
-   }
- });
+    if($('.navigation-target').hasClass('clicked')){
+      $('.navigation-target').removeClass('clicked');
+      $('.dropdown-menu').slideUp();
+    }
+    else{
+      $('.navigation-target').addClass('clicked');
+      $('.dropdown-menu').slideDown();
+    }
+  });
 
   //Fade In Images On Load 
   $('img').load(function() {
     $(this).fadeIn('slow');
   });
 
-  //Fade In Content
-  $('.content').css('visibility','visible').hide().fadeIn(1200);
+  $('.transition-link').on("click", function () {
+
+    // get the href attribute
+    var newUrl = $(this).attr("href");
+
+    // veryfy if the new url exists or is a hash
+    if (!newUrl || newUrl[0] === "#") {
+        // set that hash
+        location.hash = newUrl;
+        return;
+    }
+
+    // fade out content
+    $('html').fadeOut(300, function () {
+        // when the animation is complete, set the new location
+        location = newUrl;
+    });
+
+    // prevent the default browser behavior.
+    return false;
+  });
   
 });
 
