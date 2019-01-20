261cc350964cd0977928f96aa396ed3f639b597c
Ben Adam <bxadam@godaddy.com>
2019-01-19 21:46:36 -0700

    Try and fix error

--- plugins/blog.py
+++ plugins/blog.py
@@ -14,87 +14,92 @@ Global["config"]["post_body_block"] = "body"
 
 
 def preBuild(site):
-	
-	global Global
 
-	# Check if the posts path exists
-	page_path = os.path.join(site.page_path, Global["config"]["path"])
+    global Global
 
-	if not os.path.isdir(page_path):
-		logging.warning("No posts folder found at: %s", page_path)
+    # Check if the posts path exists
+    page_path = os.path.join(site.page_path, Global["config"]["path"])
 
-	for page in site.pages():
-		
-		if page.path.startswith("%s/" % Global["config"]["path"]):
-			
-			if not page.path.endswith('.html'):
-				continue
+    if not os.path.isdir(page_path):
+        logging.warning("No posts folder found at: %s", page_path)
 
-			context = page.context()
-			context_post = {"path": page.path}
+    for page in site.pages():
 
-			# Check if we have the required keys
-			for field in ["title", "headline", "date", "author"]:
-				
-				if not context.has_key(field):
-					logging.warning("Page %s is missing field: %s" % (page.path, field))
-				else:
-					
-					if field == "date":
-						context_post[field] = _convertDate(context[field], page.path)
-					else:
-						context_post[field] = context[field]
+        if page.path.startswith("%s/" % Global["config"]["path"]):
 
-			# Temp post context
-			temp_post_context = Context(context)
-			temp_post_context.update(context_post)
+            if not page.path.endswith('.html'):
+                continue
 
-			# Add the post contents
-			context_post["body"] = _get_node(
-				get_template(page.path), 
-				context=temp_post_context, 
-				name=Global["config"]["post_body_block"])
+            context = page.context()
+            context_post = {"path": page.path}
 
-			Global["posts"].append(context_post)
+            # Check if we have the required keys
+            for field in ["title", "headline", "date", "author"]:
 
-	# Sort the posts by date and add the next and previous page indexes
-	Global["posts"] = sorted(Global["posts"], key=lambda x: x.get("date"))
-	Global["posts"].reverse()
-	
-	indexes = xrange(0, len(Global["posts"]))
-	
-	for i in indexes:
-		if i+1 in indexes: Global["posts"][i]['prevPost'] = Global["posts"][i+1]
-		if i-1 in indexes: Global["posts"][i]['nextPost'] = Global["posts"][i-1]
+                if not context.has_key(field):
+                    logging.warning("Page %s is missing field: %s" %
+                                    (page.path, field))
+                else:
+
+                    if field == "date":
+                        context_post[field] = _convertDate(
+                            context[field], page.path)
+                    else:
+                        context_post[field] = context[field]
+
+            # Temp post context
+            temp_post_context = Context(context)
+            temp_post_context.update(context_post)
+
+            # Add the post contents
+            context_post["body"] = _get_node(
+                get_template(page.path),
+                context=temp_post_context,
+                name=Global["config"]["post_body_block"])
+
+            Global["posts"].append(context_post)
+
+    # Sort the posts by date and add the next and previous page indexes
+    Global["posts"] = sorted(Global["posts"], key=lambda x: x.get("date"))
+    Global["posts"].reverse()
+
+    indexes = xrange(0, len(Global["posts"]))
+
+    for i in indexes:
+        if i+1 in indexes:
+            Global["posts"][i]['prevPost'] = Global["posts"][i+1]
+        if i-1 in indexes:
+            Global["posts"][i]['nextPost'] = Global["posts"][i-1]
 
 
 def preBuildPage(site, page, context, data):
-	
-	context['posts'] = Global["posts"]
-	
-	for post in Global["posts"]:
-		if post["path"] == page.path:
-			context.update(post)
-	
-	return context, data
+
+    context['posts'] = Global["posts"]
+
+    for post in Global["posts"]:
+        if post["path"] == page.path:
+            context.update(post)
+
+    return context, data
 
 
 # Utilities for the functions above
 
 def _convertDate(date_string, path):
-	# Convert a string to a date object
-	try: 
-		return datetime.datetime.strptime(date_string, 
-			Global["config"]["date_format"])
-	except Exception, e:
-		logging.warning("Date format not correct for page %s, should be %s\n%s" \
-			% (path, Global["config"]["date_format"], e))
+    # Convert a string to a date object
+    try:
+        return datetime.datetime.strptime(date_string,
+                                          Global["config"]["date_format"])
+    except Exception, e:
+        logging.warning("Date format not correct for page %s, should be %s\n%s"
+                        % (path, Global["config"]["date_format"], e))
+
 
 def _get_node(template, context=Context(), name='subject'):
-	# Get the contents of a block in a specific template
-	for node in template:
-		if isinstance(node, BlockNode) and node.name == name:
-			return node.render(context)
-		elif isinstance(node, ExtendsNode):
-			return _get_node(node.nodelist, context, name)
-	raise Exception("Node '%s' could not be found in template." % name)
+    # Get the contents of a block in a specific template
+    for node in template:
+        if isinstance(node, BlockNode) and node.name == name:
+            return node.render(context)
+        elif isinstance(node, ExtendsNode):
+            return _get_node(node.nodelist, context, name)
+    raise Exception("Node '%s' could not be found in template." % name)
